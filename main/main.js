f7a0e61fe99a5f5b64c098dd55c23b9379d86457
xinyixie <xinyi.xie@thoughtworks.com>
2019-02-25 11:41:00 +0800

    1st commit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,17 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(miles, waiting) {
+    var cost;
+
+    if(miles <= 2){
+    	cost = 6; 
+    }else if(miles <= 8){
+    	cost = (miles - 2) * 0.8 + 6;	
+    }else{
+    	cost = (miles - 8) * 1.2 + (8 - 2) * 0.8 + 6;
+    }
+
+    if(waiting > 0){
+    	cost += waiting * 0.25;
+    }
+
+    return Math.round(cost);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,4 +2,37 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    it("if less than 2 miles then should be 6 yuan", function(){
+    	var miles = 1;
+    	var waiting = 0;
+    	var cost = main(miles,waiting);
+
+    	expect(cost).toEqual(6);
+    });
+
+    it("if more than 2 miles and less than 8 then should be 0.8 yuan/mile", function(){
+    	var miles = 3;
+    	var waiting = 0;
+    	var cost = main(miles,waiting);
+
+    	expect(cost).toEqual(7);
+    });
+
+    it("if more than 8 miles then should be 1.2 yuan/mile", function(){
+    	var miles = 9;
+    	var waiting = 0;
+    	var cost = main(miles,waiting);
+
+    	expect(cost).toEqual(12);
+    });
+
+    it("0.25 yuan/min for waiting time", function(){
+    	var miles = 9;
+    	var waiting = 1;
+    	var cost = main(miles, waiting);
+
+    	expect(cost).toEqual(12);
+    });
+
+
 });
